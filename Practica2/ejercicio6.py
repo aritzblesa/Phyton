5bf7c962c1bdc4458f27d2cf62e8217ad9376a71
Aritz Blesa <128011594+aritzblesa@users.noreply.github.com>
2023-03-30 16:32:19 -0300

    Add files via upload

---
+++ Practica2/ejercicio6.py
@@ -0,0 +1,7 @@
+
+palabra = input("Ingresá una palabra: ").lower()
+print(palabra)
+if "a" in palabra or "n" in palabra:
+    print("Hay letras a o letras n")
+else:
+    print("No hay letras a ni letras n ")      
